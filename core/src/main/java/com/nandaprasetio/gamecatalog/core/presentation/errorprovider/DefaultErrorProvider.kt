6e1f974086ff57223abaa6787eeb78e6e3c5103b
Nanda Prasetio <nandaprasesoft98@gmail.com>
2021-06-11 23:52:04 +0700

    Add default error provider in DefaultErrorProvider.kt.

--- core/src/main/java/com/nandaprasetio/gamecatalog/core/presentation/errorprovider/DefaultErrorProvider.kt
+++ core/src/main/java/com/nandaprasetio/gamecatalog/core/presentation/errorprovider/DefaultErrorProvider.kt
@@ -1,12 +1,14 @@
 package com.nandaprasetio.gamecatalog.core.presentation.errorprovider
 
 import android.content.Context
+import com.nandaprasetio.gamecatalog.core.exception.NotFoundException
 import java.io.IOException
 
 open class DefaultErrorProvider: ErrorProvider {
     override fun provideErrorProviderResult(t: Throwable, context: Context): ErrorProviderResult {
         return when (t) {
             is IOException -> ErrorProviderResult("Koneksi Internet Bermasalah", "Terdapat masalah pada koneksi internet. Silahkan mencoba lagi.")
+            is NotFoundException -> ErrorProviderResult("Data Tidak Ditemukan", "Untuk saat ini data tidak ditemukan.")
             else -> ErrorProviderResult("Ada Kesalahan", t.message)
         }
     }
