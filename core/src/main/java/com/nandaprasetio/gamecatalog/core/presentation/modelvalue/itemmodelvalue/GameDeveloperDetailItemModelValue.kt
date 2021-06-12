d4a543e74acb4cd68d99664db5c9babfc69b35f0
Nanda Prasetio <nandaprasesoft98@gmail.com>
2021-06-12 17:01:45 +0700

    Create game developer detail item model value.

---
+++ core/src/main/java/com/nandaprasetio/gamecatalog/core/presentation/modelvalue/itemmodelvalue/GameDeveloperDetailItemModelValue.kt
@@ -0,0 +1,7 @@
+package com.nandaprasetio.gamecatalog.core.presentation.modelvalue.itemmodelvalue
+
+import com.nandaprasetio.gamecatalog.core.domain.entity.gamedeveloper.GameDeveloperDetail
+
+class GameDeveloperDetailItemModelValue(
+    val gameDeveloperDetail: GameDeveloperDetail
+): BaseItemModelValue()
